b884a2a2d342f191a7dae9cf29de3c8f335f7444
Emmanuel Utomi <emmanuelutomi@gmail.com>
2012-09-04 16:23:20 -0500

    compile.bat: Make dirs universal no matter what your setup is

--- workspace/Project/compile.bat
+++ workspace/Project/compile.bat
@@ -1,3 +1,3 @@
 @echo off
-C:\Users\HELEN UTOMI\Desktop\gta\modmenu\bin\scocl_old.exe GTAIV "C:\Users\HELEN UTOMI\Desktop\gta\modmenu\workspace\Project\menu.c" "C:\Users\HELEN UTOMI\Desktop\gta\modmenu\workspace\Project/"
+..\..\bin\scocl_old.exe GTAIV "menu.c" "..\Project/"
 pause

0ed1691844cac301e6b1991005cf60ccc7f21caa
Emmanuel Utomi <emmanuelutomi@gmail.com>
2012-09-03 21:05:53 -0500

    compile.bat: Update dirs to my current setup

--- workspace/Project/compile.bat
+++ workspace/Project/compile.bat
@@ -1,3 +1,3 @@
 @echo off
-C:\Users\Brian\Desktop\GTASco\bin\scocl_old.exe GTAIV "C:\Users\Brian\Desktop\GTASco\workspace\Project\menu.c" "C:\Users\Brian\Desktop\GTASco\workspace\Project/"
+C:\Users\HELEN UTOMI\Desktop\gta\modmenu\bin\scocl_old.exe GTAIV "C:\Users\HELEN UTOMI\Desktop\gta\modmenu\workspace\Project\menu.c" "C:\Users\HELEN UTOMI\Desktop\gta\modmenu\workspace\Project/"
 pause

285161ac09388245e61cd5dcc46d63695807c92b
Emmanuel Utomi <emmanuelutomi@gmail.com>
2012-09-05 19:22:07 -0500

    compile.bat: Add network server error catching (if local dir changed)

--- workspace/Project/compile.bat
+++ workspace/Project/compile.bat
@@ -1,4 +1,5 @@
 @echo off
+if not exist *.c goto nofile
 if exist *.sco (
 echo Cleaning prior scripts ...
 del -f *.sco
@@ -7,6 +8,7 @@ if not defined %%1 goto compile_menu
 goto compile_custom
 
 :compile_menu
+echo.
 echo Compiling menu.sco ...
 echo.
 ..\..\bin\scocl_old.exe GTAIV "menu.c" "..\Project/"
@@ -15,6 +17,7 @@ pause
 exit 1
 
 :compile_custom
+echo.
 if %%1 NEQ *.c goto err_custom
 echo Compiling %%1 ...
 ..\..\bin\scocl_old.exe GTAIV "%%1" "..\Project/"
@@ -29,6 +32,13 @@ pause
 exit 1
 
 :err_build
+cls
 echo Your custom script did not build
+pause 
+exit 1
+
+:nofile
+cls
+echo No .c Files found in local directory
 pause 
 exit 1

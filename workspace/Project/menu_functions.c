8afec662be06bb459c55f4f179f20f850c8f096d
Emmanuel Utomi <emmanuelutomi@gmail.com>
2012-09-06 17:38:13 -0500

    menu_functions.c: More third level cleanup

--- workspace/Project/menu_functions.c
+++ workspace/Project/menu_functions.c
@@ -18,22 +18,10 @@ void second_level_functions(void){
 	}
 }
 
-void third_level_functions(void){ //these functions have there own sub id as aposed to the standard sub menus
-	if(third_level_id == 1){//Do not confuse this with the second level submenu id!!
-		if(item_select == 1){
-			prints("Third level function test.");
-		
-		}
-	
-	}
-}
-
 void menu_functions(void){
 	if(submenu_level == 1){
 		second_level_functions();
 	}
-	else if(submenu_level == 2){
-		third_level_functions();
 	}
 	else{ //main level functions go below
 		if(item_select == 2){

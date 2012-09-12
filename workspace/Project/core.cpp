cdc31f17cd8b7dec7702b6b8022e6483460f1e40
Emmanuel Utomi <emmanuelutomi@gmail.com>
2012-09-12 17:43:13 -0500

    import muskels menu 3/3

---
+++ workspace/Project/core.cpp
@@ -0,0 +1,125 @@
+/**
+	Menu core
+	by: Herr Muskelprotze
+
+**/
+
+
+
+void do_menu(void){
+	if(show_menu){
+		float pos_y = start_y;
+		
+		draw_title(0.5,0.05,0.6,0.98,s_r,s_g,s_b,255,MENU_TITLE);
+			
+		set_up_draw(3,0.35,0.35,255,255,255,255);
+		draw_text("STRING",0.05,0.975,footer);
+		
+		uint i = 1;
+		for(i;i <= item_count;i++){
+			r = 255;
+			g = 255;
+			b = 255;
+			a = 255;
+			pos_y = pos_y + 0.0375;
+			if(item_select == i){
+				r = s_r;
+				g = s_g;
+				b = s_b;
+			}
+			if(menu[i].type == 1){
+				if(menu[i].value){
+					set_up_draw(3,0.35,0.35,s_r,s_g,s_b,a);
+					draw_text("STRING",(pos_x + 0.3),pos_y,"On");
+				}
+				else{
+					set_up_draw(3,0.35,0.35,255,255,255,a);
+					draw_text("STRING",(pos_x + 0.3),pos_y,"Off");
+				}
+			}
+			if(menu[i].type == 2){
+				set_up_draw(3,0.35,0.35,s_r,s_g,s_b,a);
+				draw_text("STRING",(pos_x + 0.3),pos_y,"(");
+				
+				set_up_draw(3,0.35,0.35,255,255,255,a);				
+				draw_number("NUMBR",(pos_x + 0.305),pos_y,menu[i].value);
+				
+				set_up_draw(3,0.35,0.35,s_r,s_g,s_b,a);
+				if(menu[i].value > 99)
+					draw_text("STRING",(pos_x + 0.345),pos_y,")");
+				else if(menu[i].value > 9)
+					draw_text("STRING",(pos_x + 0.33),pos_y,")");
+				else
+					draw_text("STRING",(pos_x + 0.315),pos_y,")");
+			}
+			if(menu[i].type == 3){
+				set_up_draw(3,0.35,0.35,r,g,b,a);
+				draw_text("STRING",pos_x,pos_y,GET_STRING_FROM_TEXT_FILE(GET_DISPLAY_NAME_FROM_VEHICLE_MODEL(menu[i].value)));
+			}
+			else{
+				set_up_draw(3,0.35,0.35,r,g,b,a);
+				draw_text("STRING",pos_x,pos_y,menu[i].item_name);
+			}
+		}
+		
+		
+		
+		///Buttons
+		if(IS_BUTTON_JUST_PRESSED(0,DPAD_UP)){
+			if(item_select == 1){
+				item_select = item_count;
+			}
+			else item_select--;
+		}
+		else if(IS_BUTTON_JUST_PRESSED(0,DPAD_DOWN)){
+			if(item_select == item_count){
+				item_select = 1;
+			}
+			else item_select++;
+		}
+		else if(IS_BUTTON_JUST_PRESSED(0,DPAD_RIGHT) && menu[item_select].type == 2){
+			if(menu[item_select].value == menu[item_select].maxval)
+				menu[item_select].value = 1;
+			else
+				menu[item_select].value += 1;
+		}
+		else if(IS_BUTTON_JUST_PRESSED(0,DPAD_LEFT) && menu[item_select].type == 2){
+			if(menu[item_select].value == 1)
+				menu[item_select].value = menu[item_select].maxval;
+			else
+				menu[item_select].value -= 1;
+		}
+		else if(IS_BUTTON_JUST_PRESSED(0,BUTTON_A)){
+			if(menu[item_select].action){
+				menu_functions();
+			}
+			else{
+				uint i = 1;
+				for(i;i <= item_count;i++){
+					menu[i].action = false;
+					menu[i].type = 0;
+				}
+				last_selected[menu_level] = item_select;
+				menu_level++;
+				menu_setup();
+				item_select = 1;
+			}
+		}
+		else if(IS_BUTTON_JUST_PRESSED(0,BUTTON_B)){
+			if(menu_level > 0){
+				uint i = 1;
+				for(i;i <= item_count;i++){
+					menu[i].action = false;
+					menu[i].type = 0;
+				}
+				menu_level--;
+				item_select = last_selected[menu_level];
+				menu_setup();
+			}
+			else menu_shutdown();
+		}
+	
+	}
+	else if(IS_BUTTON_PRESSED(0,BUTTON_LB) && IS_BUTTON_JUST_PRESSED(0,DPAD_UP))
+		menu_startup();
+}
---
+++ workspace/Project/main.cpp
@@ -0,0 +1,61 @@
+#include <natives.h>
+#include <common.h>
+#include <consts.h>
+#include <types.h>
+#include <strings.h>
+
+#define VERSION "Private"
+#define MENU_TITLE "Muskelprotze's Stash"
+
+#include "vars.h"
+#include "setup.cpp"
+#include "menu_functions.cpp"
+#include "core.cpp"
+
+
+void main(void){
+	THIS_SCRIPT_IS_SAFE_FOR_NETWORK_GAME();
+	show_menu = false;
+	menu_setup();
+	s_r = 255;
+	s_g = 0;
+	s_b = 255;
+	
+	Vehicle tmp,tmp2;
+	
+	if(drive_free){
+		SWITCH_ROADS_OFF(-2225.109,-1006.106,-10,2786.717,2126.596,1000);
+		SWITCH_AMBIENT_PLANES(false);
+	}
+	else{
+		SWITCH_ROADS_ON(-2225.109,-1006.106,-10,2786.717,2126.596,1000);
+		SWITCH_AMBIENT_PLANES(true);
+	}	
+	
+	if(neverwanted)
+		SET_MAX_WANTED_LEVEL(0);
+	
+	WAIT(100);
+	do{
+		WAIT(0);
+		pPlayer = GetPlayerPed();
+		
+		do_menu();
+		looped_functions();
+		better_grenade_loop();
+		do_online_player_loop();
+		
+		if(IS_CHAR_IN_ANY_CAR(pPlayer)){
+			Ped driver;
+			SET_CHAR_WILL_FLY_THROUGH_WINDSCREEN(pPlayer,false); // Seat belt ;D
+			GET_CAR_CHAR_IS_USING(pPlayer,&tmp);
+			GET_DRIVER_OF_CAR(tmp,&driver);
+			if(driver == pPlayer){
+				GET_NETWORK_ID_FROM_VEHICLE(tmp,&tmp2);
+				SET_NETWORK_ID_CAN_MIGRATE(tmp2,false);
+			}
+		}
+		else if(HAS_CONTROL_OF_NETWORK_ID(tmp2))
+			SET_NETWORK_ID_CAN_MIGRATE(tmp2,true);
+	} while(true);
+}

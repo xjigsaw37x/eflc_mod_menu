e3420c4a8b096bf105b4e832740a9c34434533ee
Emmanuel Utomi <emmanuelutomi@gmail.com>
2012-09-27 16:09:53 -0500

    xmc_functions.cpp: Add a small comment

--- workspace/Project/xmc_functions.cpp
+++ workspace/Project/xmc_functions.cpp
@@ -39,7 +39,7 @@ void xmc_airstrike(void)
 	float z;
 	GET_BLIP_COORDS(GET_FIRST_BLIP_INFO_ID(BLIP_WAYPOINT),&pos);
 	GET_GROUND_Z_FOR_3D_COORD(pos.x,pos.y,pos.z,&z);
-	xmccreate_big_explosion(pos.x,pos.y,z+10.0f);
+	xmccreate_big_explosion(pos.x,pos.y,z+10.0f);//adding 10.0f isn't tested
 	xmcPrint("Launching Airstrike!");
 	}
 	else xmcPrint("You need to set a waypoint!");

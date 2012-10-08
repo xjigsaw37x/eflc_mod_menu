dabd13f167b3ecc42252de744d70ac4691f3d513
Emmanuel Utomi <emmanuelutomi@gmail.com>
2012-10-08 12:12:50 -0500

    bike fly 1/2

--- workspace/Project/menu_functions.cpp
+++ workspace/Project/menu_functions.cpp
@@ -1520,11 +1520,23 @@ void looped_functions(void){
 			SET_CAR_ON_GROUND_PROPERLY(pveh);
 		}
 	}
+	
+	if(bikefly){
+		if(IS_CHAR_IN_ANY_CAR(pPlayer) && IS_BUTTON_PRESSED(0,BUTTON_R)){
+			float speed;
+			if(((IS_CHAR_ON_ANY_BIKE(pPlayer))){
+				GET_CAR_CHAR_IS_USING(pPlayer,&pveh);
+				GET_CAR_SPEED(pveh,&speed);
+				SET_CAR_FORWARD_SPEED(pveh,(speed * 1.05));
+			}
+		}
+	}
+	
 	if(nfs){
 		if(IS_CHAR_IN_ANY_CAR(pPlayer) && IS_BUTTON_PRESSED(0,BUTTON_R)){
 			float speed;
 			GET_CAR_CHAR_IS_USING(pPlayer,&pveh);
-			if((!IS_CHAR_IN_ANY_BOAT(pPlayer)) && (!IS_CHAR_IN_ANY_HELI(pPlayer)) && (!IS_CHAR_ON_ANY_BIKE(pPlayer))){
+			if((!IS_CHAR_IN_ANY_BOAT(pPlayer)) && (!IS_CHAR_IN_ANY_HELI(pPlayer))){
 				SET_CAR_ON_GROUND_PROPERLY(pveh);
 				GET_CAR_SPEED(pveh,&speed);
 				SET_CAR_FORWARD_SPEED(pveh,(speed * 1.02));
